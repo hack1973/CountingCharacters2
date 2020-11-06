5c47541e5c181f0851678f51f17737eaaa5b5377
Michael Hacker <hack1973@swbell.net>
2020-11-05 23:13:33 -0600

    cleaned code from original version

---
+++ CountingCharacters2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountingCharacters2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string loremIpsumString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            char[] charactersInString = loremIpsumString.ToCharArray();
+            CountChar(charactersInString);
+        }
+
+        public static void CountChar(char[] charactersInString)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            int count = 0;
+            foreach (char item in charactersInString)
+            {
+                if (charCount.ContainsKey(item))
+                {
+                    count = charCount[item];
+                    charCount[item] = count + 1;
+                }
+                else
+                {
+                    charCount.Add(item, 1);
+                }
+            }
+
+            foreach (KeyValuePair<char, int> kvp in charCount)
+            {
+                Console.WriteLine(kvp.Key + ": " + kvp.Value);
+            }
+        }
+    }
+}
